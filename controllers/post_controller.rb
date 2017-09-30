e28ecdf789c689f0a0f5a61c7319feb2e603f040
JemimaEllis <jemimaeellis@gmail.com>
2017-10-01 00:42:22 +0100

    Basic functions

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -1,4 +1,4 @@
-class PostController < Sinatra::Base
+class MemeController < Sinatra::Base
 	configure :development do 
 		register Sinatra::Reloader
 	end
@@ -8,3 +8,58 @@ class PostController < Sinatra::Base
 	set :views, Proc.new { File.join(root, 'views')}
 
 end
+
+	#Index
+
+	get '/meme' do
+		@page_header = "All my memes"
+		@posts = Post.all
+		erb :"posts/index"
+	end
+
+	#New
+
+	get '/meme/new' do
+		erb :"posts/new"
+	end
+
+	#Create
+
+	post "/meme" do
+		new_meme = Post.new
+		new_meme.title = params[:title]
+		new_meme.body = params[:body]
+		new_meme.save
+		redirect '/meme'
+	end
+
+	#edit
+
+	get '/meme/:id/edit' do
+		@meme = Post.find(params[:id])
+
+		redirect '/posts/#{meme.id}'
+
+	end
+
+	#update
+
+	put "/meme/:id" do
+		meme = Post.find(params[:id])
+		post.title = params[:title]
+		post.body = params[:body]
+		post.save
+		redirect '/posts/#{mrmr.id'
+	end
+
+	#delete
+
+	delete 'meme/:id' do
+		Post.destroy(params[:id])
+		redirect '/meme'
+	end
+
+end
+
+
+
