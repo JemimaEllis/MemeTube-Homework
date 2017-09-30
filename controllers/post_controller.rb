ce36c5a75ae976279665e3e81eddda9db8161f28
JemimaEllis <jemimaeellis@gmail.com>
2017-09-30 22:39:01 +0100

    Set up documents so gems work

---
+++ controllers/post_controller.rb
@@ -0,0 +1,10 @@
+class PostController < Sinatra::Base
+	configure :development do 
+		register Sinatra::Reloader
+	end
+
+	set :root, File.join(File.dirname(__FILE__), "..")
+
+	set :views, Proc.new { File.join(root, 'views')}
+
+end
